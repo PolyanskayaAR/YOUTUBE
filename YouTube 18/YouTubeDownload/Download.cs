4781dfaa51d39296f6be59d67c037482e4aaa42a
Polyanskaya Anastaiya Ruslanovna <98982139+PolyanskayaAR@users.noreply.github.com>
2022-08-11 19:18:46 +0300

    Add files via upload

---
+++ YouTube 18/YouTubeDownload/Download.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeDownload.Commands
+{
+    /// <summary>
+    /// Конкретная реализация команды.
+    /// </summary>
+    class Download : Command
+    {
+        Video video;
+
+
+        public Download(Video video)
+        {
+            this.video = video;
+            Console.WriteLine("Создана команда по скачиванию видео по адресу {0}", video.Url);
+        }
+
+        // Выполнить
+        public override async void Run()
+        {
+            video.Download();
+        }
+
+        // Отменить
+        public override void Cancel()
+        {
+            Console.WriteLine("Отмена этой команды не реализована");
+        }
+    }
+}
